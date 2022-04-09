c31c92b1faaed1b1593a4ba1e3c9813dc7db4f47
aimgw <rinonodera0531@gmail.com>
2022-04-09 14:18:32 +0900

    add chmax, chmin

--- format.cpp
+++ format.cpp
@@ -4,6 +4,8 @@ typedef long long ll;
 typedef pair<int, int> P;
 typedef vector<vector<int>> G;
 #define rep(i, n) for(int i=0; i<n; i++)
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
 
 
 int main(void){
